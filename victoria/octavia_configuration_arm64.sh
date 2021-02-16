2c45e0fe2ffcac3b1994b6df3dcd8587e1ec5854
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2021-02-17 00:15:34 +0200

    Octavia Victoria fixes

--- victoria/octavia_configuration_arm64.sh
+++ victoria/octavia_configuration_arm64.sh
@@ -10,9 +10,84 @@ echo "octavia_certs_state: Oregon" | sudo tee -a /etc/kolla/globals.yml
 echo "octavia_certs_organization: OpenStack" | sudo tee -a /etc/kolla/globals.yml
 echo "octavia_certs_organizational_unit: Octavia" | sudo tee -a /etc/kolla/globals.yml
 
+cd kolla
+source venv/bin/activate
+
+sudo chown $USER:$USER /etc/kolla
 kolla-ansible octavia-certificates
 
+OCTAVIA_MGMT_SUBNET=192.168.43.0/24
+OCTAVIA_MGMT_SUBNET_START=192.168.43.10
+OCTAVIA_MGMT_SUBNET_END=192.168.43.254
+OCTAVIA_MGMT_HOST_IP=192.168.43.1/24
+OCTAVIA_MGMT_VLAN_ID=107
+
+sudo tee -a /etc/kolla/globals.yml << EOT
+octavia_amp_network:
+  name: lb-mgmt-net
+  provider_network_type: vlan
+  provider_segmentation_id: $OCTAVIA_MGMT_VLAN_ID
+  provider_physical_network: physnet1
+  external: false
+  shared: false
+  subnet:
+    name: lb-mgmt-subnet
+    cidr: "$OCTAVIA_MGMT_SUBNET"
+    allocation_pool_start: "$OCTAVIA_MGMT_SUBNET_START"
+    allocation_pool_end: "$OCTAVIA_MGMT_SUBNET_END"
+    gateway_ip: "$OCTAVIA_MGMT_HOST_IP"
+    enable_dhcp: yes
+EOT
+
+# Flavor used when booting an amphora, change as needed
+sudo tee -a /etc/kolla/globals.yml << EOT
+octavia_amp_flavor:
+  name: "amphora"
+  is_public: no
+  vcpus: 1
+  ram: 1024
+  disk: 5
+EOT
+
+# This sets up the VLAN veth interface
+# Netplan doesn't have support for veth interfaces yet
+sudo tee /usr/local/bin/veth-lbaas.sh << EOT
+#!/bin/bash
+sudo ip link add v-lbaas-vlan type veth peer name v-lbaas
+sudo ip addr add $OCTAVIA_MGMT_HOST_IP dev v-lbaas
+sudo ip link set v-lbaas-vlan up
+sudo ip link set v-lbaas up
+EOT
+sudo chmod 744 /usr/local/bin/veth-lbaas.sh
+
+sudo tee /etc/systemd/system/veth-lbaas.service << EOT
+[Unit]
+After=network.service
+
+[Service]
+ExecStart=/usr/local/bin/veth-lbaas.sh
+
+[Install]
+WantedBy=default.target
+EOT
+sudo chmod 644 /etc/systemd/system/veth-lbaas.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable veth-lbaas.service
+sudo systemctl start veth-lbaas.service
+
+docker exec openvswitch_vswitchd ovs-vsctl add-port br-ex v-lbaas-vlan tag=$OCTAVIA_MGMT_VLAN_ID
+
+echo "enable_octavia: \"yes\"" | sudo tee -a /etc/kolla/globals.yml
+echo "octavia_network_interface: v-lbaas" | sudo tee -a /etc/kolla/globals.yml
+
+# Deploy all the changes
+kolla-ansible -i all-in-one deploy --tags common,horizon,octavia
+
 # Build an ARM64 Amphora image for Octavia. Here are two alternatives: Centos or Ubuntu
+
+git clone https://github.com/cloudbase/openstack-kolla-arm64-scripts
+cd openstack-kolla-arm64-scripts/victoria
 
 # Centos
 docker build amphora-image-arm64-docker -f amphora-image-arm64-docker/Dockerfile.Centos -t amphora-image-build-arm64-centos
@@ -52,86 +127,21 @@ openstack image create amphora-x64-haproxy.qcow2 \
 --file octavia/diskimage-create/amphora-x64-haproxy.qcow2
 
 # Delete the image file
-rm octavia/diskimage-create/amphora-x64-haproxy.qcow2
-
-OCTAVIA_MGMT_SUBNET=192.168.43.0/24
-OCTAVIA_MGMT_SUBNET_START=192.168.43.10
-OCTAVIA_MGMT_SUBNET_END=192.168.43.254
-OCTAVIA_MGMT_HOST_IP=192.168.43.1/24
-OCTAVIA_MGMT_VLAN_ID=107
-
-sudo tee -a /etc/kolla/globals.yml << EOT
-octavia_amp_network:
-  name: lb-mgmt-net
-  provider_network_type: vlan
-  provider_segmentation_id: $OCTAVIA_MGMT_VLAN_ID
-  provider_physical_network: physnet1
-  external: false
-  shared: false
-  subnet:
-    name: lb-mgmt-subnet
-    cidr: "$OCTAVIA_MGMT_SUBNET"
-    allocation_pool_start: "$OCTAVIA_MGMT_SUBNET_START"
-    allocation_pool_end: "$OCTAVIA_MGMT_SUBNET_END"
-    gateway_ip: "$OCTAVIA_MGMT_HOST_IP"
-    enable_dhcp: yes
-EOT
-
-# Flavor used when booting an amphora, change as needed
-sudo tee -a /etc/kolla/globals.yml << EOT
-octavia_amp_flavor:
-  name: "amphora"
-  is_public: no
-  vcpus: 1
-  ram: 1024
-  disk: 5
-EOT
-
-openstack keypair create --private-key octavia_ssh_key octavia_ssh_key
-
-# This sets up the VLAN veth interface
-# Netplan doesn't have support for veth interfaces yet
-sudo tee /usr/local/bin/veth-lbaas.sh << EOT
-#!/bin/bash
-sudo ip link add v-lbaas-vlan type veth peer name v-lbaas
-sudo ip addr add $OCTAVIA_MGMT_HOST_IP dev v-lbaas
-sudo ip link set v-lbaas-vlan up
-sudo ip link set v-lbaas up
-EOT
-sudo chmod 744 /usr/local/bin/veth-lbaas.sh
-
-sudo tee /etc/systemd/system/veth-lbaas.service << EOT
-[Unit]
-After=network.service
-
-[Service]
-ExecStart=/usr/local/bin/veth-lbaas.sh
-
-[Install]
-WantedBy=default.target
-EOT
-sudo chmod 644 /etc/systemd/system/veth-lbaas.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable veth-lbaas.service
-sudo systemctl start veth-lbaas.service
-
-docker exec openvswitch_vswitchd ovs-vsctl add-port br-ex v-lbaas-vlan tag=$OCTAVIA_MGMT_VLAN_ID
-
-echo "enable_octavia: \"yes\"" | sudo tee -a /etc/kolla/globals.yml
-echo "octavia_network_interface: v-lbaas" | sudo tee -a /etc/kolla/globals.yml
-
-# TODO: Check if a docker restart octavia_worker is enough
-kolla-ansible -i all-in-one reconfigure
+rm -f octavia/diskimage-create/amphora-x64-haproxy.qcow2
 
 # Patch the user_data_config_drive_template
+cd octavia
 git apply  ../0001-Fix-userdata-template.patch
 docker cp octavia/common/jinja/templates/user_data_config_drive.template \
     octavia_worker:/usr/lib/python3/dist-packages/octavia/common/jinja/templates/user_data_config_drive.template
 
 # To create the loadbalancer
-openstack loadbalancer create --name loadbalancer1 --vip-subnet-id public-subnet
+. /etc/kolla/admin-openrc.sh
 
+openstack loadbalancer create --name loadbalancer1 --vip-subnet-id public1-subnet
+
+# Check status until it's marked as ONLINE
+openstack loadbalancer list
 
 # Troubleshooting
 
